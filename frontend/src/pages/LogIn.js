30eb4fc80fc6d9d32b62372122146f2c0d120c75
zgcatindig <zgcatindig@up.edu.ph>
2023-05-31 20:58:47 +0800

    feat (frontend): implement routing with auth & outlet

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,10 +1,8 @@
 
-import { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Cookies from 'universal-cookie';
+import NavBar from '../components/NavBar';
 
 export default function LogIn() {
 
@@ -14,7 +12,7 @@ export default function LogIn() {
   // redirect when login is successful
   useEffect(() => {
     if (isLoggedIn) {
-      navigate("/dashboard")
+      navigate(`/dashboard`)
     }
   }, [isLoggedIn, navigate])
 
@@ -57,13 +55,32 @@ export default function LogIn() {
   
   return (
     <>
-      <h1>Log In</h1>
-      <form id="log-in">
-        <input id="l-email" placeholder="email" />
-        <input id="l-password" type="password" placeholder="password" />
-        <button class='bg-blue' onClick={logIn}>Log In</button>
-      </form>
-      <Link to={`/`} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Sign Up</Link>
+    <NavBar/>
+
+    <div class="top-10 flex h-screen items-center self-center justify-center bg-100-payne flex-col">
+    <div class="flex w-36 h-36 rounded-full bg-black">
+      {/* <img class="rounded-full w-36 h-36" src="userimage.jpg" alt="image description">
+      </img> */}
+    </div>
+      <div className="rounded-xl px-16 py-8">
+            <div className="item-center flex flex-col px-2 pb-0 pt-0 drop-shadow-md">
+              <h1 className="flex justify-center text-5xl font-extrabold text-white mb-5">
+               Log In
+              </h1>
+            </div>
+            <form id="log-in" class="flex flex-col">
+              <input id="l-email" placeholder="Email" class=" w-96 p-2 m-2 rounded-md" />
+              <input id="l-password" type="password"  placeholder="Password"  class="p-2 m-2  rounded-md" />
+              <button class="p-2 m-2 rounded-md bg-100-charcoal text-white hover:bg-black"  onClick={logIn}>Log In</button>
+            </form> 
+      </div>
+      <div class="flex justify-center content-center ">
+        <label className="flex pl-3 text-sm text-white ">
+          Need to create an account? 
+        </label>
+        <Link to={`/signup`} class="pl-1 text-sm text-white underline">Sign Up</Link>  
+      </div>
+    </div>
 
     </>
   )
